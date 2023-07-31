6f92db95ee9d42875862bd8016434f84eab49404
Jimmy Zelinskie <jimmy@zelinskie.com>
2023-07-31 01:46:33 -0400

    hashring: avoid shadowing require

--- hashring/hashring_test.go
+++ hashring/hashring_test.go
@@ -37,34 +37,32 @@ func TestHashring(t *testing.T) {
 	for _, tc := range testCases {
 		tc := tc
 		t.Run(strconv.Itoa(int(tc.replicationFactor)), func(t *testing.T) {
-			require := require.New(t)
-
 			ring, err := NewHashring(xxhash.Sum64, tc.replicationFactor)
-			require.NoError(err)
+			require.NoError(t, err)
 
-			require.NotNil(ring.hasher)
-			require.Equal(tc.replicationFactor, ring.replicationFactor)
-			require.Len(ring.virtualNodes, 0)
-			require.Len(ring.nodes, 0)
+			require.NotNil(t, ring.hasher)
+			require.Equal(t, tc.replicationFactor, ring.replicationFactor)
+			require.Len(t, ring.virtualNodes, 0)
+			require.Len(t, ring.nodes, 0)
 
 			successfulNodes := map[string]struct{}{}
 			for _, testNodeInfo := range tc.nodes {
 				err := ring.Add(testNodeInfo)
-				require.Equal(testNodeInfo.addNodeError, err)
+				require.Equal(t, testNodeInfo.addNodeError, err)
 
 				if err == nil {
 					successfulNodes[testNodeInfo.nodeKeyAndValue] = struct{}{}
 				}
 
-				require.Len(ring.virtualNodes, len(successfulNodes)*int(tc.replicationFactor))
-				require.Len(ring.nodes, len(successfulNodes))
+				require.Len(t, ring.virtualNodes, len(successfulNodes)*int(tc.replicationFactor))
+				require.Len(t, ring.nodes, len(successfulNodes))
 
 				// Try the find function
 				if len(successfulNodes) > 0 {
 					found, err := ring.FindN([]byte("key1"), 1)
-					require.NoError(err)
-					require.Len(found, 1)
-					require.Contains(successfulNodes, found[0].Key())
+					require.NoError(t, err)
+					require.Len(t, found, 1)
+					require.Contains(t, successfulNodes, found[0].Key())
 				}
 
 				checkAllFound := map[string]struct{}{}
@@ -72,24 +70,24 @@ func TestHashring(t *testing.T) {
 					checkAllFound[k] = v
 				}
 				allFound, err := ring.FindN([]byte("key1"), uint8(len(successfulNodes)))
-				require.NoError(err)
-				require.Len(allFound, len(successfulNodes))
+				require.NoError(t, err)
+				require.Len(t, allFound, len(successfulNodes))
 
 				for _, found := range allFound {
-					require.Contains(checkAllFound, found.Key())
+					require.Contains(t, checkAllFound, found.Key())
 					delete(checkAllFound, found.Key())
 				}
 
-				require.Empty(checkAllFound)
+				require.Empty(t, checkAllFound)
 
 				// Ask for more nodes than exist
 				_, err = ring.FindN([]byte("1"), uint8(len(successfulNodes)+1))
-				require.Equal(ErrNotEnoughMembers, err)
+				require.Equal(t, ErrNotEnoughMembers, err)
 			}
 
 			// Build a consistent hash that adds the nodes in reverse order
 			reverseRing, err := NewHashring(xxhash.Sum64, tc.replicationFactor)
-			require.NoError(err)
+			require.NoError(t, err)
 
 			for i := 0; i < len(tc.nodes); i++ {
 				toAdd := tc.nodes[len(tc.nodes)-1-i]
@@ -97,7 +95,7 @@ func TestHashring(t *testing.T) {
 				// We intentionally ignore the errors here to get to the same member state
 				err := reverseRing.Add(toAdd)
 				if !errors.Is(err, ErrMemberAlreadyExists) {
-					require.Nil(err)
+					require.Nil(t, err)
 				}
 			}
 
@@ -106,12 +104,12 @@ func TestHashring(t *testing.T) {
 				for i := 0; i < 100; i++ {
 					key := []byte(strconv.Itoa(i))
 					found, err := ring.FindN(key, 1)
-					require.NoError(err)
+					require.NoError(t, err)
 
 					reverseFound, err := reverseRing.FindN(key, 1)
-					require.NoError(err)
+					require.NoError(t, err)
 
-					require.Equal(found[0].Key(), reverseFound[0].Key())
+					require.Equal(t, found[0].Key(), reverseFound[0].Key())
 				}
 			}
 
@@ -119,14 +117,14 @@ func TestHashring(t *testing.T) {
 			for _, testNodeInfo := range tc.nodes {
 				err := ring.Remove(testNodeInfo)
 				if testNodeInfo.addNodeError == nil {
-					require.NoError(err)
+					require.NoError(t, err)
 					delete(successfulNodes, testNodeInfo.nodeKeyAndValue)
 				} else {
-					require.Equal(ErrMemberNotFound, err)
+					require.Equal(t, ErrMemberNotFound, err)
 				}
 
-				require.Len(ring.virtualNodes, len(successfulNodes)*int(tc.replicationFactor))
-				require.Len(ring.nodes, len(successfulNodes))
+				require.Len(t, ring.virtualNodes, len(successfulNodes)*int(tc.replicationFactor))
+				require.Len(t, ring.nodes, len(successfulNodes))
 			}
 		})
 	}
@@ -143,26 +141,25 @@ func TestBackendBalance(t *testing.T) {
 		numMembers := numMembers
 		t.Run(strconv.Itoa(numMembers), func(t *testing.T) {
 			t.Parallel()
-			require := require.New(t)
 
 			ring, err := NewHashring(hasherFunc, 100)
-			require.NoError(err)
+			require.NoError(t, err)
 
 			memberKeyCount := map[member]int{}
 
 			for memberNum := 0; memberNum < numMembers; memberNum++ {
 				oneMember := member(memberNum)
 				err := ring.Add(oneMember)
-				require.Nil(err)
+				require.Nil(t, err)
 				memberKeyCount[oneMember] = 0
 			}
 
-			require.Len(ring.Members(), numMembers)
+			require.Len(t, ring.Members(), numMembers)
 
 			for i := 0; i < numTestKeys; i++ {
 				found, err := ring.FindN([]byte(strconv.Itoa(i)), 1)
-				require.NoError(err)
-				require.Len(found, 1)
+				require.NoError(t, err)
+				require.Len(t, found, 1)
 
 				memberKeyCount[found[0].(member)]++
 			}
@@ -174,12 +171,12 @@ func TestBackendBalance(t *testing.T) {
 				totalKeysDistributed += memberKeyCount
 				stddevSum += math.Pow(float64(memberKeyCount)-mean, 2)
 			}
-			require.Equal(numTestKeys, totalKeysDistributed)
+			require.Equal(t, numTestKeys, totalKeysDistributed)
 
 			stddev := math.Sqrt(stddevSum / float64(numMembers))
 
 			// We want the stddev to be less than 10% of the mean with 100 virtual nodes
-			require.Less(stddev, mean*.1)
+			require.Less(t, stddev, mean*.1)
 		})
 	}
 }
@@ -195,15 +192,15 @@ const (
 // it returns the mapping from before the ring was changed, the way the ring was
 // modified (add/remove/identity), and the member that was affected
 // (added, removed, or none)
-func perturb(require *require.Assertions, ring *Hashring, spread uint8,
+func perturb(tb testing.TB, ring *Hashring, spread uint8,
 	numTestKeys int) (before map[string][]Member,
 	perturbation perturbationKind, affectedMember member,
 ) {
 	before = make(map[string][]Member)
 	for i := 0; i < numTestKeys; i++ {
 		found, err := ring.FindN([]byte(strconv.Itoa(i)), spread)
-		require.NoError(err)
-		require.Len(found, int(spread))
+		require.NoError(tb, err)
+		require.Len(tb, found, int(spread))
 		before[strconv.Itoa(i)] = found
 	}
 
@@ -225,7 +222,7 @@ func perturb(require *require.Assertions, ring *Hashring, spread uint8,
 	case remove:
 		i := rand.Intn(len(ring.Members()))
 		affectedMember = ring.Members()[i].(member)
-		require.NoError(ring.Remove(affectedMember))
+		require.NoError(tb, ring.Remove(affectedMember))
 	}
 	return
 }
@@ -233,15 +230,15 @@ func perturb(require *require.Assertions, ring *Hashring, spread uint8,
 // verify takes a ring, a change that has already been applied to the ring
 // (add/remove node) and the state of the ring before the change happened, and
 // asserts that the keys were remapped correctly.
-func verify(require *require.Assertions, ring *Hashring,
+func verify(tb testing.TB, ring *Hashring,
 	before map[string][]Member, perturbation perturbationKind,
 	affectedMember member, spread uint8, numTestKeys int,
 ) {
 	for i := 0; i < numTestKeys; i++ {
 		key := strconv.Itoa(i)
 		found, err := ring.FindN([]byte(key), spread)
-		require.NoError(err)
-		require.Len(found, int(spread))
+		require.NoError(tb, err)
+		require.Len(tb, found, int(spread))
 
 		switch perturbation {
 		case remove:
@@ -250,7 +247,7 @@ func verify(require *require.Assertions, ring *Hashring,
 				if n.Key() == affectedMember.Key() {
 					continue
 				}
-				require.Contains(found, n)
+				require.Contains(tb, found, n)
 			}
 		case add:
 			// at most one key should be different,
@@ -264,50 +261,47 @@ func verify(require *require.Assertions, ring *Hashring,
 				}
 				foundMinusAffected = append(foundMinusAffected, n)
 			}
-			require.LessOrEqual(affectedCount, 1)
-			require.Subset(before[key], foundMinusAffected, "before: %#v\nafter: %#v", before[key], found)
+			require.LessOrEqual(tb, affectedCount, 1)
+			require.Subset(tb, before[key], foundMinusAffected, "before: %#v\nafter: %#v", before[key], found)
 			if len(foundMinusAffected) == len(found) {
-				require.EqualValues(found, before[key])
+				require.EqualValues(tb, found, before[key])
 			}
 		default:
-			require.Fail("invalid perturbation")
+			require.Fail(tb, "invalid perturbation")
 		}
 	}
 }
 
 func TestConsistency(t *testing.T) {
-	require := require.New(t)
-
 	ring, err := NewHashring(xxhash.Sum64, 100)
-	require.NoError(err)
+	require.NoError(t, err)
 
 	for memberNum := 0; memberNum < 5; memberNum++ {
-		require.NoError(ring.Add(member(memberNum)))
+		require.NoError(t, ring.Add(member(memberNum)))
 	}
 
 	spread := uint8(3)
 	numTestKeys := 1000
 	for i := 0; i < 10; i++ {
-		before, perturbation, affectedMember := perturb(require, ring, spread, numTestKeys)
-		verify(require, ring, before, perturbation, affectedMember, spread, numTestKeys)
+		before, perturbation, affectedMember := perturb(t, ring, spread, numTestKeys)
+		verify(t, ring, before, perturbation, affectedMember, spread, numTestKeys)
 	}
 }
 
 func BenchmarkRemapping(b *testing.B) {
-	require := require.New(b)
 	numKeys := 1000
 	numMembers := 5
 
 	ring, err := NewHashring(xxhash.Sum64, 100)
-	require.NoError(err)
+	require.NoError(b, err)
 
 	for memberNum := 0; memberNum < numMembers; memberNum++ {
-		require.NoError(ring.Add(member(memberNum)))
+		require.NoError(b, ring.Add(member(memberNum)))
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		b.StartTimer()
-		perturb(require, ring, 3, numKeys)
+		perturb(b, ring, 3, numKeys)
 		b.StopTimer()
 	}
 }
